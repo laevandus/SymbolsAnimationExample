47aa081f69160230e85ea8d086611bccfefb110f
Toomas Vahter <toomas.vahter@gmail.com>
2023-08-19 13:44:12 +0300

    Add example project

---
+++ SymbolsAnimationExample/SymbolsAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsAnimationExampleApp.swift
+//  SymbolsAnimationExample
+//
+//  Created by Toomas Vahter on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SymbolsAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
